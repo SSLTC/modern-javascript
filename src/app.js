508153a194bdf203ef9517e512e915ad13d2302a
Snoeck Sven <snoeck.sven@live.be>
2023-02-04 12:52:38 +0100

    Node env. for webpack.config

--- src/app.js
+++ src/app.js
@@ -5,117 +5,122 @@ import { removeAllChildren } from "../modules/rmAllChildElem.js";
 import { DateTime } from "luxon";
 
 // Event will start on a keyup action
-const searchBar = document.querySelector('#searchBar');
-searchBar.addEventListener('keyup', async (event) => {
+const searchBar = document.querySelector("#searchBar");
+searchBar.addEventListener("keyup", async (event) => {
+  if (event.key === "Enter") {
+    // Store target in variable
+    const thisCity = event.target.value.toLowerCase();
+    event.target.value = "";
 
-    if(event.key === "Enter") {
-        // Store target in variable
-        const thisCity = event.target.value.toLowerCase();
-        event.target.value = '';
-        
-        fetchWeatherData(thisCity);
-    }
+    fetchWeatherData(thisCity);
+  }
 });
 
 const fetchWeatherData = async (thisCity) => {
-    // Fetching first api to get the City coordinates
-    const geoData = await fetchData(`http://api.openweathermap.org/geo/1.0/direct?q=${thisCity}&appid=${Data.key}`);
+  // Fetching first api to get the City coordinates
+  const geoData = await fetchData(
+    `http://api.openweathermap.org/geo/1.0/direct?q=${thisCity}&appid=${Data.key}`
+  );
 
-    if (geoData.length === 0) {
-        // If there are errors, send out an error message
-        console.error('Error:', "not a place!");
-
-        removeAllChildren("container");
-        
-        alert("Are you sure you aren't holding your map upside down?");
-    } else {
-        const cityNameContainer = document.querySelector('.city-name');
-        cityNameContainer.innerHTML = geoData[0].name;
-
-        // Fetching data according to the coordinates     
-        let onecallData = await fetchData(`https://api.openweathermap.org/data/3.0/onecall?lat=${geoData[0].lat}&lon=${geoData[0].lon}&cnt=5&units=metric&exclude=minutely,hourly,alerts&appid=${Data.key}`);
-        if(onecallData.cod === '200') {
-            showWeatherData(onecallData);
-        }
-    }
-};
-
-const showWeatherData = (onecallData) => {
-
-    console.log('Welcome to this basic weather app. this is not a product but the product of an academic exercise.');
+  if (geoData.length === 0) {
+    // If there are errors, send out an error message
+    console.error("Error:", "not a place!");
 
     removeAllChildren("container");
 
-    // Looping through 5 days of weather data
-    for(let i = 0; i < 5; i++) {
-        createMap(onecallData, i);
+    alert("Are you sure you aren't holding your map upside down?");
+  } else {
+    const cityNameContainer = document.querySelector(".city-name");
+    cityNameContainer.innerHTML = geoData[0].name;
+
+    // Fetching data according to the coordinates
+    let onecallData = await fetchData(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${geoData[0].lat}&lon=${geoData[0].lon}&cnt=5&units=metric&exclude=minutely,hourly,alerts&appid=${Data.key}`
+    );
+    if (onecallData.cod === "200") {
+      showWeatherData(onecallData);
     }
+  }
+};
+
+const showWeatherData = (onecallData) => {
+  console.log(
+    "Welcome to this basic weather app. this is not a product but the product of an academic exercise."
+  );
+
+  removeAllChildren("container");
+
+  // Looping through 5 days of weather data
+  for (let i = 0; i < 5; i++) {
+    createMap(onecallData, i);
+  }
 };
 
 const createMap = (onecallData, index) => {
-    // const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    // Use the remainder operator (%) to switch from saturday (last in array) back to sunday (first in array)
-    // const date = new Date();
-    // let dayOfTheWeek = weekdays[(date.getDay() + index) % 7];
-    let dayOfTheWeek = DateTime.local().plus({days:`${index}`}).weekdayLong;
+  // const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  // Use the remainder operator (%) to switch from saturday (last in array) back to sunday (first in array)
+  // const date = new Date();
+  // let dayOfTheWeek = weekdays[(date.getDay() + index) % 7];
+  let dayOfTheWeek = DateTime.local().plus({ days: `${index}` }).weekdayLong;
 
-    const data = onecallData.daily[index];
+  const data = onecallData.daily[index];
 
-    // Create the elements with Data
-    const card = document.createElement('div');
-    card.classList.add("card");
+  // Create the elements with Data
+  const card = document.createElement("div");
+  card.classList.add("card");
 
-    const container = document.querySelector(".container");
-    container.appendChild(card);
+  const container = document.querySelector(".container");
+  container.appendChild(card);
 
-    const imageBox = document.createElement('div');
-    imageBox.classList.add("imgBx");
-    card.appendChild(imageBox);
+  const imageBox = document.createElement("div");
+  imageBox.classList.add("imgBx");
+  card.appendChild(imageBox);
 
-    const cardImg = document.createElement('img');
-    cardImg.src = "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
-    imageBox.appendChild(cardImg);
+  const cardImg = document.createElement("img");
+  cardImg.src =
+    "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png";
+  imageBox.appendChild(cardImg);
 
-    const contentBox = document.createElement("div");
-    contentBox.classList.add("contentBx");
-    card.appendChild(contentBox);
+  const contentBox = document.createElement("div");
+  contentBox.classList.add("contentBx");
+  card.appendChild(contentBox);
 
-    const cardHeader = document.createElement("h2");
-    cardHeader.innerHTML = dayOfTheWeek;
-    contentBox.appendChild(cardHeader);
+  const cardHeader = document.createElement("h2");
+  cardHeader.innerHTML = dayOfTheWeek;
+  contentBox.appendChild(cardHeader);
 
-    const tempDescription = document.createElement("h4");
-    tempDescription.innerHTML = data.weather[0].description;
-    contentBox.appendChild(tempDescription);
+  const tempDescription = document.createElement("h4");
+  tempDescription.innerHTML = data.weather[0].description;
+  contentBox.appendChild(tempDescription);
 
-    const currentTempBox = document.createElement("div");
-    currentTempBox.classList.add("color");
-    contentBox.appendChild(currentTempBox);
+  const currentTempBox = document.createElement("div");
+  currentTempBox.classList.add("color");
+  contentBox.appendChild(currentTempBox);
 
-    const currentTempHeader = document.createElement("h3");
-    currentTempHeader.innerHTML = "Temp:";
-    currentTempBox.appendChild(currentTempHeader);
+  const currentTempHeader = document.createElement("h3");
+  currentTempHeader.innerHTML = "Temp:";
+  currentTempBox.appendChild(currentTempHeader);
 
-    const currentTemp = document.createElement("span");
-    currentTemp.classList.add("current-temp");
-    currentTemp.innerHTML = data.temp.day + "°C";
-    currentTempBox.appendChild(currentTemp);
+  const currentTemp = document.createElement("span");
+  currentTemp.classList.add("current-temp");
+  currentTemp.innerHTML = data.temp.day + "°C";
+  currentTempBox.appendChild(currentTemp);
 
-    const minMaxTemperatures = document.createElement("div");
-    minMaxTemperatures.classList.add("details");
-    contentBox.appendChild(minMaxTemperatures);
+  const minMaxTemperatures = document.createElement("div");
+  minMaxTemperatures.classList.add("details");
+  contentBox.appendChild(minMaxTemperatures);
 
-    const minMaxTempHeader = document.createElement("h3");
-    minMaxTempHeader.innerHTML = "More:";
-    minMaxTemperatures.appendChild(minMaxTempHeader);
+  const minMaxTempHeader = document.createElement("h3");
+  minMaxTempHeader.innerHTML = "More:";
+  minMaxTemperatures.appendChild(minMaxTempHeader);
 
-    const minTemp = document.createElement("span");
-    minTemp.classList.add("min-temp");
-    minTemp.innerHTML = data.temp.min + "°C";
-    minMaxTemperatures.appendChild(minTemp);
+  const minTemp = document.createElement("span");
+  minTemp.classList.add("min-temp");
+  minTemp.innerHTML = data.temp.min + "°C";
+  minMaxTemperatures.appendChild(minTemp);
 
-    const maxTemp = document.createElement("span");
-    maxTemp.classList.add("max-temp");
-    maxTemp.innerHTML = data.temp.max + "°C";
-    minMaxTemperatures.appendChild(maxTemp);
+  const maxTemp = document.createElement("span");
+  maxTemp.classList.add("max-temp");
+  maxTemp.innerHTML = data.temp.max + "°C";
+  minMaxTemperatures.appendChild(maxTemp);
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const path = require('path');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   entry: {
-    main: path.resolve(__dirname, './src/app.js'),
-    },
-    output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'deploy'),
-    },
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                presets: ['@babel/preset-env']
-              }
-            }
+    main: path.resolve(__dirname, "./src/app.js"),
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "deploy"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
           },
-          { 
-            test: /\.css$/, 
-            use: ["style-loader", "css-loader"] 
-          },
-        ]
+        },
       },
-    devServer: {
-      static: {
-        directory: path.resolve(__dirname, 'deploy')
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
-      open: true,
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "deploy"),
     },
+    open: true,
+  },
   plugins: [
     new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: './src/index.html',
+      filename: "index.html",
+      template: "./src/index.html",
     }),
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
   ],
 };
